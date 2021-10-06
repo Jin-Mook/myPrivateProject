4209f91dd5bfd5b04e61dd09192af194965ff9b2
jinmook <jink0608@naver.com>
2021-10-06 21:13:03 +0900

    코드 간단하게 정리하기

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -8,35 +8,21 @@ function random(n) {
 
 
 function App() {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
-
-  const [othernum, setOtherNum] = useState(1);
-  const [othersum, setOtherSum] = useState(0);
-  const [othergameHistory, setOtherGameHistory] = useState([]);
+  const [myHistory, setMyHistory] = useState([])
+  const [otherHistory, setOtherHistory] = useState([])
 
 
   const handleRollClick = () => {
-    const nextNum = random(6);
+    const nextMyNum = random(6);
     const nextOtherNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum);
-    setGameHistory([...gameHistory, nextNum]);
     
-    setOtherNum(nextOtherNum);
-    setOtherSum(othersum + nextOtherNum);
-    setOtherGameHistory([...othergameHistory, nextOtherNum]);
+    setMyHistory([...myHistory, nextMyNum]);
+    setOtherHistory([...otherHistory, nextOtherNum]);
   }
 
   const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
-
-    setOtherNum(1);
-    setOtherSum(0);
-    setOtherGameHistory([]);
+    setMyHistory([]);
+    setOtherHistory([]);
   }
 
   return (
@@ -46,8 +32,8 @@ function App() {
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       <div>
-        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory}/>
-        <Board name="상대" color="red" num={othernum} sum={othersum} gameHistory={othergameHistory}/>
+        <Board name="나" color="blue" gameHistory={myHistory}/>
+        <Board name="상대" color="red" gameHistory={otherHistory}/>
       </div>
     </div>
   )
