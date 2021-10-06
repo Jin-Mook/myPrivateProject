4114f56e4b4f3bedf9e76c5950978e7a5dec7da9
jinmook <jink0608@naver.com>
2021-10-07 01:24:23 +0900

    버튼에 이벤트 등록

--- rock-sissor-paper-game/src/index.js
+++ rock-sissor-paper-game/src/index.js
@@ -1,7 +1,35 @@
 import ReactDOM from 'react-dom';
 
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+}
+
+function getResult(left, right) {
+  if (WINS[left] === right) {
+    return '승리';
+  } else if (left === WINS[right]) {
+    return '패배';
+  } else {
+    return '무승부';
+  }
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
+const me = 'rock';
+const other = 'scissor';
 
 ReactDOM.render(
-  <h2>hello world</h2>,
+  <>
+    <h1 id="title">가위바위보</h1>
+    <h2>{getResult(me, other)}</h2>
+    <button className="hand" onClick={handleClick}>가위</button>
+    <button className="hand" onClick={handleClick}>바위</button>
+    <button className="hand" onClick={handleClick}>보</button>
+  </>,
   document.getElementById('root')
 );
