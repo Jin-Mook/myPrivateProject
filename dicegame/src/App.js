2abacf265e8f8b97bfbdd926b082b9ea93af6b9f
jinmook <jink0608@naver.com>
2021-10-06 21:00:46 +0900

    Board컴포넌트를 중복사용해 나와 상대 주사위 만들고 버튼 하나로 만들기

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,27 +1,42 @@
+import Board from './Board';
 import { useState } from "react";
 import Button from "./button";
-import Dice from "./Dice";
 
 function random(n) {
   return Math.ceil(Math.random() * n);
 }
+
 
 function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0);
   const [gameHistory, setGameHistory] = useState([]);
 
+  const [othernum, setOtherNum] = useState(1);
+  const [othersum, setOtherSum] = useState(0);
+  const [othergameHistory, setOtherGameHistory] = useState([]);
+
+
   const handleRollClick = () => {
     const nextNum = random(6);
+    const nextOtherNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum);
     setGameHistory([...gameHistory, nextNum]);
+    
+    setOtherNum(nextOtherNum);
+    setOtherSum(othersum + nextOtherNum);
+    setOtherGameHistory([...othergameHistory, nextOtherNum]);
   }
 
   const handleClearClick = () => {
     setNum(1);
     setSum(0);
     setGameHistory([]);
+
+    setOtherNum(1);
+    setOtherSum(0);
+    setOtherGameHistory([]);
   }
 
   return (
@@ -31,12 +46,8 @@ function App() {
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       <div>
-        <h2>나</h2>
-        <Dice color="red" num={num}/>
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <h2>기록</h2>
-        <p>{gameHistory.join(', ')}</p>
+        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory}/>
+        <Board name="상대" color="red" num={othernum} sum={othersum} gameHistory={othergameHistory}/>
       </div>
     </div>
   )
