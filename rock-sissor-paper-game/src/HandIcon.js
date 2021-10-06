30e7718cee0544d6c4ea772dcfcea1994bdc4d81
jinmook <jink0608@naver.com>
2021-10-07 01:55:11 +0900

    HandIcon 컴포넌트의 props를 이용해 가위 바위 보 사진 넣기

--- rock-sissor-paper-game/src/HandIcon.js
+++ rock-sissor-paper-game/src/HandIcon.js
@@ -1,7 +1,26 @@
-import Img from './assets/rock.svg';
+import rockImg from './assets/rock.svg';
+import scissorImg from './assets/scissor.svg';
+import paperImg from './assets/paper.svg';
 
-function HandIcon() {
-  return <img src={Img} alt="" />
+const IMAGES = {
+  rock: rockImg,
+  scissor: scissorImg,
+  paper: paperImg,
+}
+
+function HandIcon(props) {
+  // let Img;
+  // if (props.value === 'rock') {
+  //   Img = rockImg;
+  // } else if (props.value === 'scissor') {
+  //   Img = scissorImg;
+  // } else {
+  //   Img = paperImg;
+  // }
+
+  const src = IMAGES[props.value];
+
+  return <img src={src} alt={props.value} />
 }
 
 export default HandIcon;
